1e50fb26b74ac17a60907476fa956b850e4362a8
Sowmen Das <sowmendipta@gmail.com>
2020-12-07 12:03:48 +0600

    changed detect.py

--- detect.py
+++ detect.py
@@ -102,7 +102,7 @@ def detect(save_img=False):
                 for *xyxy, conf, cls in reversed(det):
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                        line = (cls, *xyxy, conf) if opt.save_conf else (cls, *xyxy)  # label format
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
